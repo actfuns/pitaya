0e8afc7a3b33b5b1e413e84e079204fdbec54c17
parasol_qian <parasol_qian@outlook.com>
2025-03-27 15:46:07 +0800

    1

---
+++ service/task.go
@@ -0,0 +1,32 @@
+package service
+
+import (
+	"time"
+
+	"github.com/topfreegames/pitaya/v2/logger"
+	"github.com/topfreegames/pitaya/v2/thread"
+)
+
+type TaskService struct {
+	pool *thread.Pool
+}
+
+func NewTaskService(size int, workerChanCap int, expiryDurationSecond int) (*TaskService, error) {
+	pool, err := thread.NewPool(size, int32(workerChanCap), time.Duration(expiryDurationSecond)*time.Second)
+	if err != nil {
+		return nil, err
+	}
+	return &TaskService{
+		pool: pool,
+	}, nil
+}
+
+func (c *TaskService) Submit(id string, task func()) error {
+	return c.pool.Submit(id, task)
+}
+
+func (c *TaskService) Shutdown() {
+	if err := c.pool.ReleaseTimeout(time.Second * 30); err != nil {
+		logger.Log.Errorf("task service shutdown error:%v", err)
+	}
+}
