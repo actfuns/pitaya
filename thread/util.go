b6294f342bae5bf4c7beba42acfb4c81c46646fa
parasol_qian <parasol_qian@outlook.com>
2025-06-06 14:13:09 +0800

    增加一个超时检查的方法

--- thread/util.go
+++ thread/util.go
@@ -1,6 +1,10 @@
 package thread
 
 import (
+	"runtime/debug"
+	"time"
+
+	"github.com/topfreegames/pitaya/v2/logger"
 	"github.com/topfreegames/pitaya/v2/util"
 )
 
@@ -12,6 +16,21 @@ func GoSafe(fn func()) {
 // RunSafe executes a function and catches panics
 func RunSafe(fn func()) {
 	defer util.Recover()
+
+	fn()
+}
+
+// SafeRunWithTimeout executes a function and catches panics and timeouts
+func SafeRunWithTimeout(fn func(), timeout time.Duration) {
+	defer util.Recover()
+
+	start := time.Now()
+	defer func() {
+		elapsed := time.Since(start)
+		if elapsed > timeout {
+			logger.Log.Warnf("[DELAY] task timeout: took %v > %v\n%s", elapsed, timeout, debug.Stack())
+		}
+	}()
 
 	fn()
 }
