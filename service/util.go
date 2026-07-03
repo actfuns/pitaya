e7014fa7509a01df0df6987b32f22387a7863810
parasol_qian <parasol_qian@outlook.com>
2026-07-03 10:00:20 +0800

    refactor(util): 移除未使用的导入和processHandlerMessage函数

--- service/util.go
+++ service/util.go
@@ -21,21 +21,15 @@
 package service
 
 import (
-	"context"
 	"errors"
 	"reflect"
 
 	"github.com/topfreegames/pitaya/v2/component"
 	"github.com/topfreegames/pitaya/v2/conn/message"
 	"github.com/topfreegames/pitaya/v2/constants"
-	e "github.com/topfreegames/pitaya/v2/errors"
 	"github.com/topfreegames/pitaya/v2/logger"
-	"github.com/topfreegames/pitaya/v2/logger/interfaces"
-	"github.com/topfreegames/pitaya/v2/pipeline"
 	"github.com/topfreegames/pitaya/v2/protos"
-	"github.com/topfreegames/pitaya/v2/route"
 	"github.com/topfreegames/pitaya/v2/serialize"
-	"github.com/topfreegames/pitaya/v2/session"
 	"github.com/topfreegames/pitaya/v2/util"
 	"google.golang.org/protobuf/proto"
 )
@@ -97,75 +91,4 @@ func serializeReturn(ser serialize.Serializer, ret interface{}) ([]byte, error) 
 		}
 	}
 	return res, nil
-}
-
-func processHandlerMessage(
-	ctx context.Context,
-	rt *route.Route,
-	handler *component.Handler,
-	serializer serialize.Serializer,
-	handlerHooks *pipeline.HandlerHooks,
-	session session.Session,
-	data []byte,
-	msgTypeIface interface{},
-	remote bool,
-) ([]byte, error) {
-	if ctx == nil {
-		ctx = context.Background()
-	}
-	ctx = context.WithValue(ctx, constants.SessionCtxKey, session)
-	ctx = util.CtxWithDefaultLogger(ctx, rt.Short(), session.UID())
-
-	msgType, err := getMsgType(msgTypeIface)
-	if err != nil {
-		return nil, e.NewError(err, e.ErrInternalCode)
-	}
-
-	logger := ctx.Value(constants.LoggerCtxKey).(interfaces.Logger)
-	exit, err := handler.ValidateMessageType(msgType)
-	if err != nil && exit {
-		return nil, e.NewError(err, e.ErrBadRequestCode)
-	} else if err != nil {
-		logger.Warnf("invalid message type, error: %s", err.Error())
-	}
-
-	// First unmarshal the handler arg that will be passed to
-	// both handler and pipeline functions
-	arg, err := unmarshalHandlerArg(handler, serializer, data)
-	if err != nil {
-		return nil, e.NewError(err, e.ErrBadRequestCode)
-	}
-
-	ctx, arg, err = handlerHooks.BeforeHandler.ExecuteBeforePipeline(ctx, arg)
-	if err != nil {
-		return nil, err
-	}
-
-	logger.Debugf("SID=%d, Data=%s", session.ID(), data)
-	args := []reflect.Value{handler.Receiver, reflect.ValueOf(ctx)}
-	if arg != nil {
-		args = append(args, reflect.ValueOf(arg))
-	}
-
-	resp, err := util.Pcall(handler.Method, args)
-	if remote && msgType == message.Notify {
-		// This is a special case and should only happen with nats rpc client
-		// because we used nats request we have to answer to it or else a timeout
-		// will happen in the caller server and will be returned to the client
-		// the reason why we don't just Publish is to keep track of failed rpc requests
-		// with timeouts, maybe we can improve this flow
-		resp = []byte("ack")
-	}
-
-	resp, err = handlerHooks.AfterHandler.ExecuteAfterPipeline(ctx, resp, err)
-	if err != nil {
-		return nil, err
-	}
-
-	ret, err := serializeReturn(serializer, resp)
-	if err != nil {
-		return nil, err
-	}
-
-	return ret, nil
 }
